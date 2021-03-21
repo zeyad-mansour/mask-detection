ae2125ff11a45da4ec48f8b79b7afd18d9df069b
Zeyad Mansour <45726273+zeyad-mansour@users.noreply.github.com>
2021-03-21 02:21:56 -0400

    Delete score-calculator.py

--- score-calculator.py
+++
@@ -1,6 +0,0 @@
-def calc_score(num_ppl: int, avg_dist: float):
-  if num_ppl <= 1: return 5
-  min_avg = num_ppl ** 2.0 / 4000.0 + 6.0
-  if num_ppl > 30: min_avg *= 1.1
-  return max(min((avg_dist / min_avg) * 10.0 - 5.0, 5.0), 0.0)
-  
